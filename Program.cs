518c4b57773372534669045d9c1b63fdbffb8f9c
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-23 14:34:47 +0700

    Xay Dung Lop Stop Watch

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XayDungLopStopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.Reset();
+            stopWatch.Start();
+            SelectionSort();
+            stopWatch.Stop();
+            stopWatch.GetElapsedTimePerMillisecond();
+            Console.WriteLine("\nRunTime: " + stopWatch.elapsedTime);
+        }
+
+        static void SelectionSort()
+        {
+            Random random = new Random();
+            int[] arr = new int[100000];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(0, 100000);
+            }
+
+            Console.WriteLine("====================================================================================================");
+            Console.WriteLine("Initial array is: ");
+            Console.WriteLine("====================================================================================================");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Array.Sort(arr);
+
+            Console.WriteLine();
+            Console.WriteLine("====================================================================================================");
+            Console.WriteLine("Sorted array is: ");
+            Console.WriteLine("====================================================================================================");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
---
+++ StopWatch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XayDungLopStopWatch
+{
+    public class StopWatch
+    {
+        private double startTime, endTime;
+        public double elapsedTime;
+
+        public void Reset()
+        {
+            startTime = 0;
+        }
+        public void Start()
+        {
+            startTime = DateTime.Now.Ticks;
+        }
+
+        public void Stop()
+        {
+            endTime = DateTime.Now.Ticks;
+            elapsedTime = endTime - startTime;
+        }
+
+        public void GetElapsedTimePerMillisecond()
+        {
+            elapsedTime = elapsedTime / 10000;
+        }
+    }
+}
